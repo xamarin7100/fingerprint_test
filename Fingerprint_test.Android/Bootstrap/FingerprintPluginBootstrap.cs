a14bdb5786a7720ca48bd4fa69ba980272648426
xamarin7100 <losteven@yahoo.com>
2017-05-19 09:32:43 -0400

    Add files via upload

---
+++ Fingerprint_test.Android/Bootstrap/FingerprintPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+namespace Fingerprint_test.Droid.Bootstrap
+{
+    public class FingerprintPluginBootstrap
+        : MvxPluginBootstrapAction<MvvmCross.Plugins.Fingerprint.PluginLoader>
+    {
+    }
+}
